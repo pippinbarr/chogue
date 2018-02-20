25e11f34d6b02f271fe5161372a00df5c639379d
Pippin Barr <pippin.barr@gmail.com>
2018-02-20 13:01:48 -0500

    Added beginning of doors (not very successful)

--- Assets/Scripts/RoomGen.cs
+++ Assets/Scripts/RoomGen.cs
@@ -9,8 +9,8 @@ public class RoomGen : MonoBehaviour
     public int m_LevelHeight = 25;
 
     public int padding = 1;
-    public int minRoomWidth = 2;
-    public int minRoomHeight = 2;
+    public int minRoomWidth = 3;
+    public int minRoomHeight = 3;
     public int hallSize = 2;
 
     private int roomZoneWidth;
@@ -19,11 +19,18 @@ public class RoomGen : MonoBehaviour
     private int maxRoomWidth;
     private int maxRoomHeight;
 
+    private Room[][] rooms;
+
+    private Vector2[][] hDoors;
+    private Vector2[][] vDoors;
+     
     public Texture2D m_Level;
 
 
     void Start()
     {
+        rooms = new Room[][] { new Room[3], new Room[3], new Room[3] };
+
         // Calculate how big the space a room can occupy is
         // (a third of the screen - the two hallways)
         roomZoneWidth = (m_LevelWidth - 2 * hallSize) / 3;
@@ -46,31 +53,125 @@ public class RoomGen : MonoBehaviour
         // Every pixel is black by default, so we "only" need to fill the texture
         // with space
 
+        CreateRooms();
+
+
+        for (int col = 0; col < 3; col++)
+        {
+            for (int row = 0; row < 3; row++)
+            {
+                AddDoors(rooms[col][row]);
+                DrawRoom(rooms[col][row]);
+                DrawHalls(rooms[col][row]);
+            }
+        }
+
+        //m_Level.SetPixel(0,0,Color.white);
+
+    }
+
+    private void CreateRooms()
+    {
         for (int row = 0; row < 3; row++)
         {
             for (int col = 0; col < 3; col++)
             {
-                AddRoom(row * (roomZoneWidth + hallSize) + padding, col * (roomZoneHeight + hallSize) + padding);
+                CreateRoom(row,col);
             }
         }
-
     }
 
-    private void AddRoom(int roomX, int roomY)
+    private void CreateRoom(int row, int col)
     {
         int width = minRoomWidth + Mathf.FloorToInt(Random.value * (maxRoomWidth - minRoomWidth));
         int height = minRoomHeight + Mathf.FloorToInt(Random.value * (maxRoomHeight - minRoomHeight));
-        width = maxRoomWidth;
-        height = maxRoomHeight;
+        //width = maxRoomWidth;
+        //height = maxRoomHeight;
+        int x = col * (roomZoneWidth + hallSize) + padding;
+        int y = row * (roomZoneHeight + hallSize) + padding;
+        rooms[col][row] = new Room(row, col, x, y, width, height);
+    }
 
-        for (int y = roomY; y < roomY + height; y++)
+    private void AddDoors(Room room)
+    {
+        if (room.row + 1 <= 2) room.doors[room.SOUTH] = rooms[room.col][room.row + 1];
+        if (room.col + 1 <= 2) room.doors[room.EAST] = rooms[room.col + 1][room.row];
+    }
+
+    private void DrawRoom(Room room)
+    {
+
+        for (int y = room.y; y <= room.y + room.height; y++)
         {
-            for (int x = roomX; x < roomX + width; x++)
+            for (int x = room.x; x <= room.x + room.width; x++)
             {
                 m_Level.SetPixel(x,y,Color.white);
             }
         }
     }
 
+    private void DrawHalls(Room room)
+    {
+        Room eastRoom = room.doors[room.EAST];
+        if (eastRoom != null) {
+            int startX = room.x + room.width;
+            int startY = room.y + Mathf.FloorToInt(Random.value * (room.height - 1));
+            int endX = eastRoom.x;
+            int endY = eastRoom.y + Mathf.FloorToInt((Random.value * (eastRoom.height - 1)));
 
+            int currentX = startX;
+            int currentY = startY;
+
+            int moveX = 1;
+            int moveY = 0;
+            while (currentX < endX/2)
+            {
+                m_Level.SetPixel(currentX, currentY, Color.white);
+                currentX++;
+            }
+            while (currentY != endY)
+            {
+                if (endY - currentY < 0) currentY--;
+                else if (endY - currentY > 0) currentY++;
+                m_Level.SetPixel(currentX, currentY, Color.white);
+            }
+            //while (currentX < endX)
+            //{
+            //    m_Level.SetPixel(currentX, currentY, Color.white);
+            //    currentX += moveX;
+            //}
+
+            //m_Level.SetPixel(endX - 1, endY,Color.white);
+        }
+    }
+
+
+}
+
+class Room
+{
+    public int row;
+    public int col;
+    public int x;
+    public int y;
+    public int width;
+    public int height;
+
+    public int WEST = 0;
+    public int EAST = 1;
+    public int NORTH = 2;
+    public int SOUTH = 3;
+
+    public Room[] doors;
+
+    public Room(int _row, int _col, int _x, int _y, int _width, int _height)
+    {
+        row = _row;
+        col = _col;
+        x = _x;
+        y = _y;
+        width = _width;
+        height = _height;
+        doors = new Room[4];
+    }
 }
