79a8d7c34eb6c2cdf9a0c6e9cccc9cd336020957
Pippin Barr <pippin.barr@gmail.com>
2019-06-05 19:31:33 -0400

    Fixed GameOver tombstone scaling across devices? (A small script for it in ScaleCameraToDevice that calculates desirable width based on the tombstone)

---
+++ Assets/ScaleCameraToDevice.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleCameraToDevice : MonoBehaviour
+{
+    private Camera m_Camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Camera = GetComponent<Camera>();
+
+        float unitsPerPixel = 25f / Screen.width;
+
+        float desiredHalfHeight = 0.5f * unitsPerPixel * Screen.height;
+
+        m_Camera.orthographicSize = desiredHalfHeight;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float unitsPerPixel = 23f / Screen.width;
+
+        float desiredHalfHeight = 0.5f * unitsPerPixel * Screen.height;
+
+        m_Camera.orthographicSize = desiredHalfHeight;
+
+    }
+}
